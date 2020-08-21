2015896d12c4f9abcb1ca61a67c1f92d123053ab
Erich Richter <erich.richter@gmail.com>
2020-08-21 10:52:46 -0700

    add controller routes

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
-
+using Microsoft.EntityFrameworkCore;
 namespace AnimalShelter.Controllers
 {
   [Route("api/[controller]")]
@@ -23,6 +23,13 @@ namespace AnimalShelter.Controllers
       return _db.Animals.ToList();
     }
 
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+        return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+    
     // POST api/animals
     [HttpPost]
     public void Post([FromBody] Animal animal)
@@ -30,5 +37,25 @@ namespace AnimalShelter.Controllers
       _db.Animals.Add(animal);
       _db.SaveChanges();
     }
+  
+    // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+        animal.AnimalId = id;
+        _db.Entry(animal).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+  
+      // DELETE api/animals/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
+    }
+  
+  
   }
 }
